4b94eddb98922900608b4cbc15e80fe5f142dd92
Elvir9 <ehalilovicc@gmail.com>
2021-06-08 17:17:53 +0200

    Creating API for write json object in file system

--- src/routes/writePostsInFile.js
+++ src/routes/writePostsInFile.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  res.send('Hello softhouse!');
-});
+const ctrlPosts = require('../controllers/posts.controllers');
 
+router.route('/').post(ctrlPosts.writeFile);
 module.exports = router;
